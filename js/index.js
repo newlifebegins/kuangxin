8ba23c95b5887023d85b3d2f8c2511ed4bad95c6
weiwang <1345795498@qq.com>
2016-12-08 19:05:07 +0800

    init

--- js/index.js
+++ js/index.js
@@ -9,6 +9,22 @@ $(function(){
 	$("#product").carousel({
 		interval:false,
 	})
+
+	$("#application1").carousel({
+		interval:false,
+	})
+
+	$("#application2").carousel({
+		interval:false,
+	})
+
+	$("#application3").carousel({
+		interval:false,
+	})
+
+	$("#application4").carousel({
+		interval:false,
+	})
    
 })
 
@@ -17,5 +33,11 @@ $(document).ready(function(){
 		var index = $(this).index();
 		$(this).addClass("active").siblings(".product").removeClass("active");
 		$("#product").find(".item").eq(index).addClass("active").siblings(".item").removeClass("active");
+	})
+
+	$(".application .row").find("div").on("click",function(){
+		var index = $(this).index();
+		$(this).addClass("active").siblings("div").removeClass("active");
+		$(".application").find(".app-col").eq(index).addClass("active").siblings(".item").removeClass("active");
 	})
 })
