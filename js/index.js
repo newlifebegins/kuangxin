2ee6a40c8faf1b8e1d54d09625cf1edf174eec03
weiwang <1345795498@qq.com>
2016-12-08 16:57:21 +0800

    init

--- js/index.js
+++ js/index.js
@@ -5,5 +5,17 @@ $(function(){
 		interval:4100,
 	})
 	
+	// 产品轮播自动播放
+	$("#product").carousel({
+		interval:false,
+	})
    
+})
+
+$(document).ready(function(){
+	$(".product-nav").find(".product").on("click",function(){
+		var index = $(this).index();
+		$(this).addClass("active").siblings(".product").removeClass("active");
+		$("#product").find(".item").eq(index).addClass("active").siblings(".item").removeClass("active");
+	})
 })
